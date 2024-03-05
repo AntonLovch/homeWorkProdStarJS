7d572d0ef8bfa6117f53948d168b9bc45135b28c
Anton <lovchij_a@mail.ru>
2024-03-05 22:10:21 +0100

    regExp HomeWork

--- script.js
+++ script.js
@@ -48,3 +48,44 @@ document.getElementById('btn2').innerHTML = t('btn2');
 document.getElementById('accept').innerHTML = t('accept');
 });
 }
+
+const input = document.getElementById('mail');
+input.addEventListener('input', onInput);
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+
+
+function onInput() {
+  if (isEmailValid(input.value)) {
+    input.style.borderColor = 'green';
+    input.style.color = 'green';
+  } else {
+    input.style.borderColor = 'red';
+    input.style.color = 'red';
+  }
+}
+
+function isEmailValid(value) {
+return EMAIL_REGEXP.test(value)};
+
+
+
+const input2 = document.getElementById('names');
+input2.addEventListener('input', onInput2);
+
+const NAME_REGEXP = /^([А-Я]{1}[а-яё]{1,20}[ ][А-Я]{1}[а-яё]{1,20})([-][А-Я]{1}[а-яё]{2,22})?/iu;
+
+
+function onInput2() {
+  if (isNameslValid(input2.value)) {
+    input2.style.borderColor = 'green';
+    input2.style.color = 'green';
+  } else {
+    input2.style.borderColor = 'red';
+    input2.style.color = 'red';
+  }
+}
+
+function isNameslValid(value) {
+return NAME_REGEXP.test(value)};
